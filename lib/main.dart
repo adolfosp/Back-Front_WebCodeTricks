ad7c81f4463fe27f5f8bf2fcd09e251eb03d5961
Adolfo <adolfo.poiatti@fatec.sp.gov.br>
2020-09-07 17:31:45 -0300

    Estilizando a tela de login principal

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:webcodetricks/pageInicial.dart';
 import 'package:webcodetricks/screen/login_screen.dart';
+import 'package:webcodetricks/Animation/FadeAnimation.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -41,67 +42,192 @@ class LoginPageWidgetState extends State<LoginPageWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: new BoxDecoration(
-            image: new DecorationImage(
-          image: AssetImage("images/Logo.png"),
-          fit: BoxFit.cover,
-        )),
-        padding: EdgeInsets.all(50),
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                onPressed: () {
-                  onGoogleSignIn(context);
-                },
-                color: isUserSignedIn ? Colors.green : Colors.blueAccent,
-                child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(Icons.account_circle, color: Colors.white),
-                        SizedBox(width: 10),
-                        Text(
-                            isUserSignedIn
-                                ? 'Você já está logado'
-                                : 'Logar com Google',
-                            style: TextStyle(color: Colors.white))
-                      ],
-                    )),
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [Colors.grey, Colors.grey[400], Colors.grey[300]])),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 80,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(
+                      1,
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                            fontFamily: "NotoSans"),
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FadeAnimation(
+                      1.3,
+                      Text(
+                        "Seja Bem-vindo ",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      )),
+                ],
               ),
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginScreen()),
-                  );
-                },
-                color: isUserSignedIn ? Colors.green : Colors.blueAccent,
-                child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60))),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(40),
+                    child: Column(
                       children: <Widget>[
-                        Icon(Icons.account_circle, color: Colors.white),
-                        SizedBox(width: 10),
-                        Text("Logar como Dev",
-                            style: TextStyle(color: Colors.white))
+                        SizedBox(
+                          height: 60,
+                        ),
+                        FadeAnimation(
+                          1.4,
+                          SizedBox(
+                              width: 500,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color:
+                                              Color.fromRGBO(105, 105, 105, .7),
+                                          blurRadius: 20,
+                                          offset: Offset(0, 10))
+                                    ]),
+                                child: Column(
+                                  children: <Widget>[
+                                    SizedBox(
+                                      height: 60,
+                                      width: 300,
+                                      child: FlatButton(
+                                          padding: EdgeInsets.all(0.0),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                          onPressed: () {
+                                            onGoogleSignIn(context);
+                                          },
+                                          color: isUserSignedIn
+                                              ? Colors.blueAccent
+                                              : Colors.blueAccent,
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: <Widget>[
+                                              Image.asset(
+                                                'images/google.png',
+                                                height: 60.0,
+                                              ),
+                                              SizedBox(width: 18),
+                                              SizedBox(
+                                                  width: 170,
+                                                  child: Text(
+                                                      isUserSignedIn
+                                                          ? 'Você já está logado'
+                                                          : 'Logar com a conta da Google',
+                                                      textAlign:
+                                                          TextAlign.center,
+                                                      style: TextStyle(
+                                                        color: Colors.white,
+                                                      ))),
+                                            ],
+                                          )),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 60,
+                          width: 300,
+                        ),
+                        FadeAnimation(
+                            1.4,
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color:
+                                            Color.fromRGBO(105, 105, 105, .7),
+                                        blurRadius: 20,
+                                        offset: Offset(0, 10))
+                                  ]),
+                              child: Column(
+                                children: <Widget>[
+                                  SizedBox(
+                                    height: 60,
+                                    width: 300,
+                                    child: FlatButton(
+                                        padding: EdgeInsets.all(0.0),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      LoginScreen()));
+                                        },
+                                        color: Colors.blueAccent,
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: <Widget>[
+                                            SizedBox(width: 4),
+                                            Icon(
+                                              Icons.account_circle,
+                                              color: Colors.white,
+                                              size: 52,
+                                            ),
+                                            SizedBox(width: 18),
+                                            SizedBox(
+                                                width: 170,
+                                                child: Text("Logar como Autor",
+                                                    textAlign: TextAlign.center,
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                    ))),
+                                          ],
+                                        )),
+                                  ),
+                                ],
+                              ),
+                            )),
+                        SizedBox(
+                          height: 40,
+                        ),
                       ],
-                    )),
+                    ),
+                  ),
+                ),
               ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
